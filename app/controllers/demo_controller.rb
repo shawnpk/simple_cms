6bf3a3266587ee6b1d30fccbc0e47098a07b04c3
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-10-17 09:43:00 -0700

    Add links to pages

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -5,13 +5,28 @@ class DemoController < ApplicationController
   def index
   	# render(:template => 'demo/hello')
   	# render('demo/hello')
-  	render('hello')
+  	# render('hello')
+
+
   end
 
   def hello
   	# render(:template => 'demo/index')
   	# render('demo/index')
-  	render('index')
+  	# render('index')
+
+  	@array = [1, 2, 3, 4, 5]
+  	@id = params[:id].to_i # params come back as strings. Must convert them to integers to do math.
+  	@page = params['page'].to_i # params come back as strings. Must convert them to integers to do math.
+  end
+
+  def other_hello
+  	# redirect_to(:controller => 'demo', :action => 'index')
+  	redirect_to(:action => 'index')
+  end
+
+  def inept
+  	redirect_to('http://ineptsoftware.com')
   end
 
 end
