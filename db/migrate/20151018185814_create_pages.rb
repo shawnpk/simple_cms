057729b7867639bee6657ca1810ec3be036fbcad
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-10-18 12:41:25 -0700

    Add Subject, Pages and Section models

---
+++ db/migrate/20151018185814_create_pages.rb
@@ -0,0 +1,23 @@
+class CreatePages < ActiveRecord::Migration
+  
+  def up
+    create_table :pages do |t|
+      t.integer :subject_id # can also use t.references :subject instead
+      t.string :name
+      t.string :permalink
+      t.integer :position
+      t.boolean :visible, default: false
+
+      t.timestamps null: false
+    end
+
+    add_index("pages", "subject_id")
+    add_index("pages", "permalink")
+
+  end
+
+  def down
+    drop_table :pages
+  end
+
+end
---
+++ db/migrate/20151018185819_create_sections.rb
@@ -0,0 +1,27 @@
+class CreateSections < ActiveRecord::Migration
+
+  # "change" runs the commands as given when migrating "up",
+  # runs the reverse of the commands when migrating "down".
+  # Only works if all commands are reversible.
+  # For example:
+  #   add_column can be reversed as remove_column
+  #     it has all info remove_column needs.
+  #   remove_column can't be reversed as add_column
+  #     it doesn't have column devinition add_column needs.
+
+  def change
+    create_table :sections do |t|
+      t.integer :page_id # can also use t.references :page
+      t.string :name
+      t.integer :position
+      t.boolean :visible, default: false
+      t.string :content_type
+      t.text :content
+
+      t.timestamps null: false
+    end
+
+    add_index("sections", "page_id")
+
+  end
+end
